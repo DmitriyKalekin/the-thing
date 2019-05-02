c86aeaee49ea1044816c2fee83bd8391a107043e
KalekinDmitry <d.kalekin@humaniq.com>
2019-05-03 05:17:43 +0600

    quart server https + runner

--- api/server.py
+++ api/server.py
@@ -1,5 +1,7 @@
-
 from quart import Quart
+# from flask import Flask
+
+
 app = Quart(__name__)
 app.url_map.strict_slashes = False
 app.config['DEBUG'] = True
--- main.py
+++ main.py
@@ -1,20 +1,20 @@
-import asyncio
-import aiomysql
+# import asyncio
+# import aiomysql
 from api.server import app
 from api.views import *
 from api.jobs import jobs_loop
 from config import CFG
 
-async def main(loop):
-    app.CFG = CFG
-    app.loop = loop
-    asyncio.ensure_future(jobs_loop())
+# async def main(loop):
+#     app.CFG = CFG
+#     app.loop = loop
+#     asyncio.ensure_future(jobs_loop())
 
 
 if __name__ == "__main__":
     try:
-        loop = asyncio.get_event_loop()
-        loop.run_until_complete(main(loop))
-        app.run(host='127.0.0.1', port=5000, debug=True)
+        # loop = asyncio.get_event_loop()
+        # loop.run_until_complete(main(loop))
+        app.run()
     except KeyboardInterrupt:
         print("Exiting")
